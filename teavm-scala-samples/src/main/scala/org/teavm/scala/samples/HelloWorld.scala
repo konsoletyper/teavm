03afce4a1262acc57ff9a585baf62d925db9c3f5
konsoletyper <konsoletyper@gmail.com>
2014-03-16 12:17:27 +0400

    Adds experimental sample of Scala code. Unfortunately, TeaVM can't compile it due to missing JCL classes.

---
+++ teavm-scala-samples/src/main/scala/org/teavm/scala/samples/HelloWorld.scala
@@ -0,0 +1,11 @@
+package org.teavm.scala.samples
+
+/**
+ *
+ * @author Alexey Andreev <konsoletyper@gmail.com>
+ */
+object HelloWorld {
+    def main(args : Array[String]) {
+        print("Hello")
+    }
+}
