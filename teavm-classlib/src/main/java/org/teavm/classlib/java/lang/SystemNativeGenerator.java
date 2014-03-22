8e99b4589ef083602e316ebe62920922a31da736
konsoletyper <konsoletyper@gmail.com>
2014-03-22 11:32:39 +0400

    Fixes System.arraycopy behavior when src == dest

--- teavm-classlib/src/main/java/org/teavm/classlib/java/lang/SystemNativeGenerator.java
+++ teavm-classlib/src/main/java/org/teavm/classlib/java/lang/SystemNativeGenerator.java
@@ -74,8 +74,16 @@ public class SystemNativeGenerator implements Generator, DependencyPlugin {
         String dest = context.getParameterName(3);
         String destPos = context.getParameterName(4);
         String length = context.getParameterName(5);
+        writer.append("if (" + src + " !== " +  dest + " || " + destPos + " < " + srcPos + ") {").indent().newLine();
         writer.append("for (var i = 0; i < " + length + "; i = (i + 1) | 0) {").indent().softNewLine();
         writer.append(dest + ".data[" + destPos + "++] = " + src + ".data[" + srcPos + "++];").softNewLine();
+        writer.outdent().append("}").softNewLine();
+        writer.outdent().append("} else {").indent().softNewLine();
+        writer.append(srcPos + " = (" + srcPos + " + " + length + ") | 0;").softNewLine();
+        writer.append(destPos + " = (" + destPos + " + " + length + ") | 0;").softNewLine();
+        writer.append("for (var i = 0; i < " + length + "; i = (i + 1) | 0) {").indent().softNewLine();
+        writer.append(dest + ".data[--" + destPos + "] = " + src + ".data[--" + srcPos + "];").softNewLine();
+        writer.outdent().append("}").softNewLine();
         writer.outdent().append("}").softNewLine();
     }
 
