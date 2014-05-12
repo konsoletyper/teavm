2308d5409962e9f73a960f6ebea314f9e6dccfac
konsoletyper <konsoletyper@gmail.com>
2014-05-12 17:41:50 +0400

    Adds locale implementation using CLDR data

---
+++ teavm-classlib/src/main/java/org/teavm/classlib/java/util/LocaleNativeGenerator.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2014 Alexey Andreev.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.teavm.classlib.java.util;
+
+import java.io.IOException;
+import org.teavm.codegen.SourceWriter;
+import org.teavm.javascript.ni.Generator;
+import org.teavm.javascript.ni.GeneratorContext;
+import org.teavm.model.MethodReference;
+
+/**
+ *
+ * @author Alexey Andreev
+ */
+public class LocaleNativeGenerator implements Generator {
+    @Override
+    public void generate(GeneratorContext context, SourceWriter writer, MethodReference methodRef) throws IOException {
+        switch (methodRef.getName()) {
+            case "getDisplayCountry":
+                writer.append("var result = ").appendClass("java.util.Locale").append(".$CLDR[$rt_ustr(")
+                        .append(context.getParameterName(1)).append(")].territories[$rt_ustr(")
+                        .append(context.getParameterName(2)).append(")];").softNewLine();
+                writer.append("return result ? $rt_str(result) : null").softNewLine();
+                break;
+            case "getDisplayLanguage":
+                writer.append("var result = ").appendClass("java.util.Locale").append(".$CLDR[$rt_ustr(")
+                .append(context.getParameterName(1)).append(")].languages[$rt_ustr(")
+                .append(context.getParameterName(2)).append(")];").softNewLine();
+                writer.append("return result ? $rt_str(result) : null;").softNewLine();
+                break;
+            case "getAvailableLocales":
+                generateAvailableLocales(writer);
+                break;
+        }
+    }
+
+    private void generateAvailableLocales(SourceWriter writer) throws IOException {
+        writer.append("var locales = Object.keys(").appendClass("java.util.Locale").append(".$CLDR);").softNewLine();
+        writer.append("var array = $rt_createArray(").appendClass("java.lang.String").append(", locales);")
+                .softNewLine();
+        writer.append("for (var i = 0; i < locales.length; ++i) {").indent().softNewLine();
+        writer.append("array.data[i] = locales[i];");
+    }
+}
