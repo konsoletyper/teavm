5c8b80aa32e8f3b589ce3c76a20fd3282b73dffa
konsoletyper <konsoletyper@gmail.com>
2014-10-26 17:26:21 +0300

    Add BufferedInputStream and StringTokenizer

---
+++ teavm-classlib/src/test/java/org/teavm/classlib/java/io/BufferedInputStreamTest.java
@@ -0,0 +1,338 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.teavm.classlib.java.io;
+
+import static org.junit.Assert.*;
+import java.io.*;
+import org.junit.Test;
+
+@SuppressWarnings("resource")
+public class BufferedInputStreamTest {
+    byte[] ibuf = new byte[4096];
+
+    @Test
+    public void test_ConstructorLjava_io_InputStream() {
+        try {
+            BufferedInputStream str = new BufferedInputStream(null);
+            str.read();
+            fail("Expected an IOException");
+        } catch (IOException e) {
+            // Expected
+        }
+    }
+
+    static class MockBufferedInputStream extends BufferedInputStream {
+        static byte[] buf;
+
+        MockBufferedInputStream(InputStream is) {
+            super(is);
+            buf = super.buf;
+        }
+
+        MockBufferedInputStream(InputStream is, int size) {
+            super(is, size);
+            buf = super.buf;
+        }
+    }
+
+    @Test
+    public void test_available() throws IOException {
+        ByteArrayInputStream isFile = new ByteArrayInputStream(new byte[] { 2, 3, 5, 7, 11 });
+        BufferedInputStream is = new BufferedInputStream(isFile);
+        assertTrue("Returned incorrect number of available bytes", is.available() == 5);
+
+        // Test that a closed stream throws an IOE for available()
+        BufferedInputStream bis = new BufferedInputStream(new ByteArrayInputStream(new byte[] { 'h', 'e', 'l', 'l',
+                'o', ' ', 't', 'i', 'm' }));
+        int available = bis.available();
+        bis.close();
+        assertTrue(available != 0);
+
+        try {
+            bis.available();
+            fail("Expected test to throw IOE.");
+        } catch (IOException ex) {
+            // expected
+        }
+    }
+
+    @Test
+    public void test_close() throws IOException {
+        new BufferedInputStream(new ByteArrayInputStream(new byte[100])).close();
+
+        // regression for HARMONY-667
+        BufferedInputStream buf = new BufferedInputStream(null, 5);
+        buf.close();
+    }
+
+    @Test
+    public void test_markI() throws IOException {
+        ByteArrayInputStream isFile = new ByteArrayInputStream(new byte[10000]);
+        BufferedInputStream is = new BufferedInputStream(isFile, 5);
+        byte[] buf1 = new byte[100];
+        byte[] buf2 = new byte[100];
+        is.skip(3000);
+        is.mark(1000);
+        is.read(buf1, 0, buf1.length);
+        is.reset();
+        is.read(buf2, 0, buf2.length);
+        is.reset();
+        assertTrue("Failed to mark correct position",
+                new String(buf1, 0, buf1.length).equals(new String(buf2, 0, buf2.length)));
+
+        byte[] bytes = new byte[256];
+        for (int i = 0; i < 256; i++) {
+            bytes[i] = (byte)i;
+        }
+        InputStream in = new BufferedInputStream(new ByteArrayInputStream(bytes), 12);
+        in.skip(6);
+        in.mark(14);
+        in.read(new byte[14], 0, 14);
+        in.reset();
+        assertTrue("Wrong bytes", in.read() == 6 && in.read() == 7);
+
+        in = new BufferedInputStream(new ByteArrayInputStream(bytes), 12);
+        in.skip(6);
+        in.mark(8);
+        in.skip(7);
+        in.reset();
+        assertTrue("Wrong bytes 2", in.read() == 6 && in.read() == 7);
+
+        BufferedInputStream buf = new BufferedInputStream(new ByteArrayInputStream(new byte[] { 0, 1, 2, 3, 4 }), 2);
+        buf.mark(3);
+        bytes = new byte[3];
+        int result = buf.read(bytes);
+        assertEquals(3, result);
+        assertEquals("Assert 0:", 0, bytes[0]);
+        assertEquals("Assert 1:", 1, bytes[1]);
+        assertEquals("Assert 2:", 2, bytes[2]);
+        assertEquals("Assert 3:", 3, buf.read());
+
+        buf = new BufferedInputStream(new ByteArrayInputStream(new byte[] { 0, 1, 2, 3, 4 }), 2);
+        buf.mark(3);
+        bytes = new byte[4];
+        result = buf.read(bytes);
+        assertEquals(4, result);
+        assertEquals("Assert 4:", 0, bytes[0]);
+        assertEquals("Assert 5:", 1, bytes[1]);
+        assertEquals("Assert 6:", 2, bytes[2]);
+        assertEquals("Assert 7:", 3, bytes[3]);
+        assertEquals("Assert 8:", 4, buf.read());
+        assertEquals("Assert 9:", -1, buf.read());
+
+        buf = new BufferedInputStream(new ByteArrayInputStream(new byte[] { 0, 1, 2, 3, 4 }), 2);
+        buf.mark(Integer.MAX_VALUE);
+        buf.read();
+        buf.close();
+    }
+
+    @Test
+    public void test_read() throws IOException {
+        ByteArrayInputStream isFile = new ByteArrayInputStream(new byte[10000]);
+        BufferedInputStream is = new BufferedInputStream(isFile, 5);
+        InputStreamReader isr = new InputStreamReader(is);
+        int c = isr.read();
+        assertTrue("read returned incorrect char", c == 0);
+
+        byte[] bytes = new byte[256];
+        for (int i = 0; i < 256; i++) {
+            bytes[i] = (byte)i;
+        }
+        InputStream in = new BufferedInputStream(new ByteArrayInputStream(bytes), 12);
+        assertEquals("Wrong initial byte", 0, in.read()); // Fill the
+        // buffer
+        byte[] buf = new byte[14];
+        in.read(buf, 0, 14); // Read greater than the buffer
+        assertTrue("Wrong block read data", new String(buf, 0, 14).equals(new String(bytes, 1, 14)));
+        assertEquals("Wrong bytes", 15, in.read()); // Check next byte
+    }
+
+    @Test
+    public void test_read$BII_Exception() throws IOException {
+        BufferedInputStream bis = new BufferedInputStream(null);
+
+        try {
+            bis.read(new byte[0], -1, -1);
+            fail("should throw IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            // expected
+        }
+
+        try {
+            bis.read(new byte[0], 1, -1);
+            fail("should throw IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            // expected
+        }
+
+        try {
+            bis.read(new byte[0], 1, 1);
+            fail("should throw IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            // expected
+        }
+
+        bis.close();
+
+        try {
+            bis.read(null, -1, -1);
+            fail("should throw IOException");
+        } catch (IOException e) {
+            // expected
+        }
+    }
+
+    @Test
+    public void test_read$BII() throws IOException {
+        ByteArrayInputStream isFile = new ByteArrayInputStream(new byte[10000]);
+        BufferedInputStream is = new BufferedInputStream(isFile);
+        byte[] buf1 = new byte[100];
+        is.skip(3000);
+        is.mark(1000);
+        is.read(buf1, 0, buf1.length);
+
+        BufferedInputStream bufin = new BufferedInputStream(new InputStream() {
+            int size = 2, pos = 0;
+
+            byte[] contents = new byte[size];
+
+            @Override
+            public int read() throws IOException {
+                if (pos >= size) {
+                    throw new IOException("Read past end of data");
+                }
+                return contents[pos++];
+            }
+
+            @Override
+            public int read(byte[] buf, int off, int len) throws IOException {
+                if (pos >= size) {
+                    throw new IOException("Read past end of data");
+                }
+                int toRead = len;
+                if (toRead > available()) {
+                    toRead = available();
+                }
+                System.arraycopy(contents, pos, buf, off, toRead);
+                pos += toRead;
+                return toRead;
+            }
+
+            @Override
+            public int available() {
+                return size - pos;
+            }
+        });
+        bufin.read();
+        int result = bufin.read(new byte[2], 0, 2);
+        assertTrue("Incorrect result: " + result, result == 1);
+    }
+
+    @Test
+    public void test_reset() throws IOException {
+        ByteArrayInputStream isFile = new ByteArrayInputStream(new byte[10000]);
+        BufferedInputStream is = new BufferedInputStream(isFile);
+        byte[] buf1 = new byte[10];
+        byte[] buf2 = new byte[10];
+        is.mark(2000);
+        is.read(buf1, 0, 10);
+        is.reset();
+        is.read(buf2, 0, 10);
+        is.reset();
+        assertTrue("Reset failed", new String(buf1, 0, buf1.length).equals(new String(buf2, 0, buf2.length)));
+
+        BufferedInputStream bIn = new BufferedInputStream(new ByteArrayInputStream("1234567890".getBytes()));
+        bIn.mark(10);
+        for (int i = 0; i < 11; i++) {
+            bIn.read();
+        }
+        bIn.reset();
+    }
+
+    @Test
+    public void test_reset_Exception() throws IOException {
+        BufferedInputStream bis = new BufferedInputStream(null);
+
+        // throws IOException with message "Mark has been invalidated"
+        try {
+            bis.reset();
+            fail("should throw IOException");
+        } catch (IOException e) {
+            // expected
+        }
+
+        // does not throw IOException
+        bis.mark(1);
+        bis.reset();
+
+        bis.close();
+
+        // throws IOException with message "stream is closed"
+        try {
+            bis.reset();
+            fail("should throw IOException");
+        } catch (IOException e) {
+            // expected
+        }
+    }
+
+    @Test
+    public void test_reset_scenario1() throws IOException {
+        byte[] input = "12345678900".getBytes();
+        BufferedInputStream buffis = new BufferedInputStream(new ByteArrayInputStream(input));
+        buffis.read();
+        buffis.mark(5);
+        buffis.skip(5);
+        buffis.reset();
+    }
+
+    @Test
+    public void test_reset_scenario2() throws IOException {
+        byte[] input = "12345678900".getBytes();
+        BufferedInputStream buffis = new BufferedInputStream(new ByteArrayInputStream(input));
+        buffis.mark(5);
+        buffis.skip(6);
+        buffis.reset();
+    }
+
+    @Test
+    public void test_skipJ() throws IOException {
+        ByteArrayInputStream isFile = new ByteArrayInputStream(new byte[10000]);
+        BufferedInputStream is = new BufferedInputStream(isFile);
+
+        byte[] buf1 = new byte[10];
+        is.mark(2000);
+        is.skip(1000);
+        is.read(buf1, 0, buf1.length);
+        is.reset();
+
+        // regression for HARMONY-667
+        try {
+            BufferedInputStream buf = new BufferedInputStream(null, 5);
+            buf.skip(10);
+            fail("Should throw IOException");
+        } catch (IOException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void test_skip_NullInputStream() throws IOException {
+        BufferedInputStream buf = new BufferedInputStream(null, 5);
+        assertEquals(0, buf.skip(0));
+    }
+}
---
+++ teavm-classlib/src/test/java/org/teavm/classlib/java/util/StringTokenizerTest.java
@@ -0,0 +1,194 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.teavm.classlib.java.util;
+
+import static org.junit.Assert.*;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+import org.junit.Test;
+
+public class StringTokenizerTest {
+    @Test
+    public void test_ConstructorLjava_lang_StringLjava_lang_String() {
+        StringTokenizer st = new StringTokenizer("This:is:a:test:String", ":");
+        assertTrue("Created incorrect tokenizer", st.countTokens() == 5 && (st.nextElement().equals("This")));
+    }
+
+    @Test
+    public void test_ConstructorLjava_lang_StringLjava_lang_StringZ() {
+        StringTokenizer st = new StringTokenizer("This:is:a:test:String", ":", true);
+        st.nextElement();
+        assertTrue("Created incorrect tokenizer", st.countTokens() == 8 && (st.nextElement().equals(":")));
+    }
+
+    @Test
+    public void test_countTokens() {
+        StringTokenizer st = new StringTokenizer("This is a test String");
+        assertEquals("Incorrect token count returned", 5, st.countTokens());
+    }
+
+    @Test
+    public void test_hasMoreElements() {
+        StringTokenizer st = new StringTokenizer("This is a test String");
+        st.nextElement();
+        assertTrue("hasMoreElements returned incorrect value", st.hasMoreElements());
+        st.nextElement();
+        st.nextElement();
+        st.nextElement();
+        st.nextElement();
+        assertTrue("hasMoreElements returned incorrect value", !st.hasMoreElements());
+    }
+
+    @Test
+    public void test_hasMoreTokens() {
+        StringTokenizer st = new StringTokenizer("This is a test String");
+        for (int counter = 0; counter < 5; counter++) {
+            assertTrue("StringTokenizer incorrectly reports it has no more tokens", st.hasMoreTokens());
+            st.nextToken();
+        }
+        assertTrue("StringTokenizer incorrectly reports it has more tokens", !st.hasMoreTokens());
+    }
+
+    @Test
+    public void test_nextElement() {
+        StringTokenizer st = new StringTokenizer("This is a test String");
+        assertEquals("nextElement returned incorrect value", "This", st.nextElement());
+        assertEquals("nextElement returned incorrect value", "is", st.nextElement());
+        assertEquals("nextElement returned incorrect value", "a", st.nextElement());
+        assertEquals("nextElement returned incorrect value", "test", st.nextElement());
+        assertEquals("nextElement returned incorrect value", "String", st.nextElement());
+        try {
+            st.nextElement();
+            fail("nextElement failed to throw a NoSuchElementException when it should have been out of elements");
+        } catch (NoSuchElementException e) {
+            return;
+        }
+    }
+
+    @Test
+    public void test_nextToken() {
+        StringTokenizer st = new StringTokenizer("This is a test String");
+        assertEquals("nextToken returned incorrect value", "This", st.nextToken());
+        assertEquals("nextToken returned incorrect value", "is", st.nextToken());
+        assertEquals("nextToken returned incorrect value", "a", st.nextToken());
+        assertEquals("nextToken returned incorrect value", "test", st.nextToken());
+        assertEquals("nextToken returned incorrect value", "String", st.nextToken());
+        try {
+            st.nextToken();
+            fail("nextToken failed to throw a NoSuchElementException when it should have been out of elements");
+        } catch (NoSuchElementException e) {
+            return;
+        }
+    }
+
+    @Test
+    public void test_nextTokenLjava_lang_String() {
+        StringTokenizer st = new StringTokenizer("This is a test String");
+        assertEquals("nextToken(String) returned incorrect value with normal token String", "This", st.nextToken(" "));
+        assertEquals("nextToken(String) returned incorrect value with custom token String", " is a ",
+                st.nextToken("tr"));
+        assertEquals("calling nextToken() did not use the new default delimiter list", "es", st.nextToken());
+    }
+
+    @Test
+    public void test_hasMoreElements_NPE() {
+        StringTokenizer stringTokenizer = new StringTokenizer(new String(), (String)null, true);
+        try {
+            stringTokenizer.hasMoreElements();
+            fail("should throw NullPointerException");
+        } catch (NullPointerException e) {
+            // Expected
+        }
+
+        stringTokenizer = new StringTokenizer(new String(), (String)null);
+        try {
+            stringTokenizer.hasMoreElements();
+            fail("should throw NullPointerException");
+        } catch (NullPointerException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void test_hasMoreTokens_NPE() {
+        StringTokenizer stringTokenizer = new StringTokenizer(new String(), (String)null, true);
+        try {
+            stringTokenizer.hasMoreTokens();
+            fail("should throw NullPointerException");
+        } catch (NullPointerException e) {
+            // Expected
+        }
+
+        stringTokenizer = new StringTokenizer(new String(), (String)null);
+        try {
+            stringTokenizer.hasMoreTokens();
+            fail("should throw NullPointerException");
+        } catch (NullPointerException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void test_nextElement_NPE() {
+        StringTokenizer stringTokenizer = new StringTokenizer(new String(), (String)null, true);
+        try {
+            stringTokenizer.nextElement();
+            fail("should throw NullPointerException");
+        } catch (NullPointerException e) {
+            // Expected
+        }
+
+        stringTokenizer = new StringTokenizer(new String(), (String)null);
+        try {
+            stringTokenizer.nextElement();
+            fail("should throw NullPointerException");
+        } catch (NullPointerException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void test_nextToken_NPE() {
+        StringTokenizer stringTokenizer = new StringTokenizer(new String(), (String)null, true);
+        try {
+            stringTokenizer.nextToken();
+            fail("should throw NullPointerException");
+        } catch (NullPointerException e) {
+            // Expected
+        }
+
+        stringTokenizer = new StringTokenizer(new String(), (String)null);
+        try {
+            stringTokenizer.nextToken();
+            fail("should throw NullPointerException");
+        } catch (NullPointerException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void test_nextTokenLjava_lang_String_NPE() {
+        StringTokenizer stringTokenizer = new StringTokenizer(new String());
+        try {
+            stringTokenizer.nextToken(null);
+            fail("should throw NullPointerException");
+        } catch (NullPointerException e) {
+            // Expected
+        }
+    }
+}
