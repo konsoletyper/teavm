01f07edd1af76feb39b52dd196abdb60ca979652
Mike Poindexter <staticsnow@gmail.com>
2015-07-20 21:56:13 -0700

    Change copyright year

--- teavm-dom/src/main/java/org/teavm/dom/media/SourceBuffer.java
+++ teavm-dom/src/main/java/org/teavm/dom/media/SourceBuffer.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2014 Alexey Andreev.
+ *  Copyright 2015 Alexey Andreev.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
