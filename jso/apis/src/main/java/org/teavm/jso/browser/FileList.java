26517f9fa8c1ee0c1b3eeb2b50286ff76ef359db
jfwittmann <jfwittmann7@googlemail.com>
2015-10-25 22:59:10 +0100

    add jso File interface and update input element

---
+++ jso/apis/src/main/java/org/teavm/jso/browser/FileList.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2015 Alexey Andreev.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.teavm.jso.browser;
+
+import org.teavm.jso.JSIndexer;
+import org.teavm.jso.JSObject;
+import org.teavm.jso.JSProperty;
+
+/**
+*
+* @author Jan-Felix Wittmann
+*/
+public interface FileList extends JSObject {
+
+    @JSProperty
+    int getLength();
+
+    @JSIndexer
+    File get(int index);
+
+    File item(int index);
+
+}
