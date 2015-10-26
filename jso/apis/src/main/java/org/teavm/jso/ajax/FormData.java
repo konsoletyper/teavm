790158b692c06b5af6c0724cf4e61aadc10d1756
jfwittmann <jfwittmann7@googlemail.com>
2015-10-26 12:26:13 +0100

    Update send method in XMLHttpRequest and add FormData type

---
+++ jso/apis/src/main/java/org/teavm/jso/ajax/FormData.java
@@ -0,0 +1,56 @@
+/*
+ *  Copyright 2014 Alexey Andreev.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.teavm.jso.ajax;
+
+import org.teavm.jso.JSBody;
+import org.teavm.jso.JSObject;
+import org.teavm.jso.blob.Blob;
+import org.teavm.jso.core.JSArray;
+
+/**
+*
+* @author Jan-Felix Wittmann
+*/
+public abstract class FormData implements JSObject {
+
+    public abstract void append(String name, String value);
+
+    public abstract void append(String name, String value, String filename);
+
+    public abstract <T extends Blob> void append(String name, T value);
+
+    public abstract <T extends Blob> void append(String name, T value, String filename);
+
+    public abstract void delete(String name);
+
+    public abstract JSObject get(String name);
+
+    public abstract JSArray<JSObject> getAll(String name);
+
+    public abstract boolean has(String name);
+
+    public abstract void set(String name, String value);
+
+    public abstract void set(String name, String value, String filename);
+
+    public abstract <T extends Blob> void set(String name, T value);
+
+    public abstract <T extends Blob> void set(String name, T value, String filename);
+
+    @JSBody(params = {}, script = "return new FormData();")
+    public static native FormData create();
+
+}
