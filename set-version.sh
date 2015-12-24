697bc5ec6c8cbfc758473a76857f0df42525366e
Alexey Andreev <konsoletyper@gmail.com>
2015-12-24 22:10:02 +0300

    Developing set-version script

--- set-version.sh
+++ set-version.sh
@@ -2,7 +2,15 @@
 
 mvn versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
 pushd tools/eclipse
-mvn tycho-versions:set-version -DnewVersion=$1
-cd core-plugin
+mvn org.eclipse.tycho:tycho-versions-plugin:0.21.0:set-version -DnewVersion=$1
+
+pushd core-plugin
 mvn -f dep-pom.xml versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
+sed -r -i -e "s/lib\/(teavm(-[a-z]+)+)-.+\.jar/\1-$1.jar/" build.properties
+sed -r -i -e "s/lib\/(teavm(-[a-z]+)+)-.+\.jar/\1-$1.jar/" META-INF/MANIFEST.MF
+popd
+
+pushd m2e-plugin
+sed -r -i -e "s/<versionRange>.+<\/versionRange>/<versionRange>$1<\/versionRange>/" lifecycle-mapping-metadata.xml
+popd
 popd
