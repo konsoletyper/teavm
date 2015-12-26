8851794485fb6f2cb3a95fa17a7c5ffce3a5b4a4
Alexey Andreev <konsoletyper@gmail.com>
2015-12-26 14:24:43 +0300

    Fix set-version script

--- set-version.sh
+++ set-version.sh
@@ -5,9 +5,9 @@ pushd tools/eclipse
 mvn org.eclipse.tycho:tycho-versions-plugin:0.21.0:set-version -DnewVersion=$1
 
 pushd core-plugin
-mvn -f dep-pom.xml versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
-sed -r -i -e "s/lib\/(teavm(-[a-z]+)+)-.+\.jar/\1-$1.jar/" build.properties
-sed -r -i -e "s/lib\/(teavm(-[a-z]+)+)-.+\.jar/\1-$1.jar/" META-INF/MANIFEST.MF
+sed -r -i -e "s/<version><!-- update -->(.+)<\/version>/<version><!-- update -->$1<\/version>/" dep-pom.xml
+sed -r -i -e "s/(lib\/teavm(-[a-z]+)+)-.+\.jar/\1-$1.jar/" build.properties
+sed -r -i -e "s/(lib\/teavm(-[a-z]+)+)-.+\.jar/\1-$1.jar/" META-INF/MANIFEST.MF
 popd
 
 pushd m2e-plugin
