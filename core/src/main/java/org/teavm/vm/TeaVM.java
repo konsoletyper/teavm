1ae384dc667faf21b4ed28e7daeb6889d6b9b085
bertung <bora.ertung@gmail.com>
2016-01-28 11:01:23 +0200

    let the TeaVM know about the target file name and directory

--- core/src/main/java/org/teavm/vm/TeaVM.java
+++ core/src/main/java/org/teavm/vm/TeaVM.java
@@ -92,6 +92,8 @@ public class TeaVM implements TeaVMHost, ServiceRepository {
     private ListableClassHolderSource writtenClasses;
     private Set<MethodReference> asyncMethods = new HashSet<>();
     private Set<MethodReference> asyncFamilyMethods = new HashSet<>();
+    private File targetDirectory;
+    private String targetFileName;
 
     TeaVM(ClassReaderSource classSource, ClassLoader classLoader) {
         this.classSource = classSource;
@@ -106,7 +108,23 @@ public class TeaVM implements TeaVMHost, ServiceRepository {
             }
         };
     }
+    
+    public File getTargetDirectory() {
+        return targetDirectory;
+    }
 
+    public void setTargetDirectory(File targetDirectory) {
+        this.targetDirectory = targetDirectory;
+    }
+
+    public String getTargetFileName() {
+        return targetFileName;
+    }
+
+    public void setTargetFileName(String targetFileName) {
+        this.targetFileName = targetFileName;
+    }
+    
     @Override
     public void add(DependencyListener listener) {
         dependencyChecker.addDependencyListener(listener);
