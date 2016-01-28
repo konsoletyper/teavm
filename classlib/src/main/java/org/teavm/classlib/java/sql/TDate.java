da2358beed9aa6f62a038e6583f6b2a5f10380d7
bertung <bora.ertung@gmail.com>
2016-01-28 11:22:26 +0200

    Update TDate.java

--- classlib/src/main/java/org/teavm/classlib/java/sql/TDate.java
+++ classlib/src/main/java/org/teavm/classlib/java/sql/TDate.java
@@ -34,25 +34,4 @@ public class TDate extends org.teavm.classlib.java.util.TDate {
     public void setTime(long date) {
         super.setTime(date);
     }
-
-    @Override
-    public String toString () {
-        char buf[] = "2000-00-00".toCharArray();
-        
-        int year = super.getYear() + 1900;
-        buf[0] = Character.forDigit(year/1000,10);
-        buf[1] = Character.forDigit((year/100)%10,10);
-        buf[2] = Character.forDigit((year/10)%10,10);
-        buf[3] = Character.forDigit(year%10,10);
-        
-        int month = super.getMonth() + 1;
-        buf[5] = Character.forDigit(month/10,10);
-        buf[6] = Character.forDigit(month%10,10);
-        
-        int day = super.getDate();
-        buf[8] = Character.forDigit(day/10,10);
-        buf[9] = Character.forDigit(day%10,10);
-
-        return new String(buf);
-    }
 }
