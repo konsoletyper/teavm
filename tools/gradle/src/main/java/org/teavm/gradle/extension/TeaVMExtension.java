01ffdaee5b97f04d5cd639ac2af8980f7e0cd4c3
MJ <john.hervicc@gmail.com>
2016-03-09 15:30:32 +0100

    Added license and all tool properties. Updated example.

--- tools/gradle/src/main/java/org/teavm/gradle/extension/TeaVMExtension.java
+++ tools/gradle/src/main/java/org/teavm/gradle/extension/TeaVMExtension.java
@@ -1,41 +1,58 @@
+/*
+ *  Copyright 2016 MJ.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.teavm.gradle.extension;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.teavm.tooling.RuntimeCopyOperation;
 
+/** POJO file with TeaVM plugin settings.
+ *
+ * @author MJ
+ * @see <a href=
+ *      "https://github.com/konsoletyper/teavm/wiki/Building-JavaScript-with-Maven-and-TeaVM#the-build-javascript-goal">
+ *      Maven properties</a> */
 public class TeaVMExtension {
-    private String targetDirectory = "dist";
-    private String cacheDirectory = "teavm-cache";
-    private String targetFile = "app.js";
+    private boolean includeDependencies = true;
     private String version = "0.4.3";
+    private String targetDirectory = "javascript";
+    private String targetFileName = "classes.js";
+    private boolean minifying = true;
+    private boolean debugInformationGenerated;
+    private boolean sourceMapsGenerated;
+    private boolean sourceFilesCopied;
+    private boolean incremental;
+    private String cacheDirectory = "teavm-cache";
+    private Map<String, String> properties = new HashMap<String, String>();
     private RuntimeCopyOperation runtime = RuntimeCopyOperation.SEPARATE;
-    private boolean minify = true;
-    private boolean debugInfo = true;
-    private boolean sourceMaps = true;
-    private boolean sourceFilesCopied = true;
-    private boolean mainPageIncluded = true;
+    private boolean mainPageIncluded;
+    private List<String> transformers;
+    private Map<String, String> classAliases = new HashMap<String, String>();
+    private List<Map<String, Object>> methodAliases = new ArrayList<Map<String, Object>>();
 
-    public String getTargetDirectory() {
-        return targetDirectory;
+    public boolean isIncludeDependencies() {
+        return includeDependencies;
     }
 
-    public void setTargetDirectory(final String targetDirectory) {
-        this.targetDirectory = targetDirectory;
-    }
-
-    public String getCacheDirectory() {
-        return cacheDirectory;
-    }
-
-    public void setCacheDirectory(final String cacheDirectory) {
-        this.cacheDirectory = cacheDirectory;
-    }
-
-    public String getTargetFile() {
-        return targetFile;
-    }
-
-    public void setTargetFile(final String targetFile) {
-        this.targetFile = targetFile;
+    public void setIncludeDependencies(final boolean includeDependencies) {
+        this.includeDependencies = includeDependencies;
     }
 
     public String getVersion() {
@@ -46,36 +63,44 @@ public class TeaVMExtension {
         this.version = version;
     }
 
-    public RuntimeCopyOperation getRuntime() {
-        return runtime;
+    public String getTargetDirectory() {
+        return targetDirectory;
     }
 
-    public void setRuntime(final RuntimeCopyOperation runtime) {
-        this.runtime = runtime;
+    public void setTargetDirectory(final String targetDirectory) {
+        this.targetDirectory = targetDirectory;
     }
 
-    public boolean isMinify() {
-        return minify;
+    public String getTargetFileName() {
+        return targetFileName;
     }
 
-    public void setMinify(final boolean minify) {
-        this.minify = minify;
+    public void setTargetFileName(final String targetFileName) {
+        this.targetFileName = targetFileName;
     }
 
-    public boolean isDebugInfo() {
-        return debugInfo;
+    public boolean isMinifying() {
+        return minifying;
     }
 
-    public void setDebugInfo(final boolean debugInfo) {
-        this.debugInfo = debugInfo;
+    public void setMinifying(final boolean minifying) {
+        this.minifying = minifying;
     }
 
-    public boolean isSourceMaps() {
-        return sourceMaps;
+    public boolean isDebugInformationGenerated() {
+        return debugInformationGenerated;
     }
 
-    public void setSourceMaps(final boolean sourceMaps) {
-        this.sourceMaps = sourceMaps;
+    public void setDebugInformationGenerated(final boolean debugInformationGenerated) {
+        this.debugInformationGenerated = debugInformationGenerated;
+    }
+
+    public boolean isSourceMapsGenerated() {
+        return sourceMapsGenerated;
+    }
+
+    public void setSourceMapsGenerated(final boolean sourceMapsGenerated) {
+        this.sourceMapsGenerated = sourceMapsGenerated;
     }
 
     public boolean isSourceFilesCopied() {
@@ -86,11 +111,67 @@ public class TeaVMExtension {
         this.sourceFilesCopied = sourceFilesCopied;
     }
 
+    public boolean isIncremental() {
+        return incremental;
+    }
+
+    public void setIncremental(final boolean incremental) {
+        this.incremental = incremental;
+    }
+
+    public String getCacheDirectory() {
+        return cacheDirectory;
+    }
+
+    public void setCacheDirectory(final String cacheDirectory) {
+        this.cacheDirectory = cacheDirectory;
+    }
+
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(final Map<String, String> properties) {
+        this.properties = properties;
+    }
+
+    public RuntimeCopyOperation getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(final RuntimeCopyOperation runtime) {
+        this.runtime = runtime;
+    }
+
     public boolean isMainPageIncluded() {
         return mainPageIncluded;
     }
 
     public void setMainPageIncluded(final boolean mainPageIncluded) {
         this.mainPageIncluded = mainPageIncluded;
+    }
+
+    public List<String> getTransformers() {
+        return transformers;
+    }
+
+    public void setTransformers(final List<String> transformers) {
+        this.transformers = transformers;
+    }
+
+    public Map<String, String> getClassAliases() {
+        return classAliases;
+    }
+
+    public void setClassAliases(final Map<String, String> classAliases) {
+        this.classAliases = classAliases;
+    }
+
+    public List<Map<String, Object>> getMethodAliases() {
+        return methodAliases;
+    }
+
+    public void setMethodAliases(final List<Map<String, Object>> methodAliases) {
+        this.methodAliases = methodAliases;
     }
 }
--- tools/gradle/src/main/java/org/teavm/gradle/task/CompileTask.java
+++ tools/gradle/src/main/java/org/teavm/gradle/task/CompileTask.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2016 MJ.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.teavm.gradle.task;
 
 import java.io.File;
@@ -6,23 +22,44 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.TaskAction;
 import org.teavm.gradle.TeaVMPlugin;
+import org.teavm.gradle.extension.MethodAliasUtils;
 import org.teavm.gradle.extension.TeaVMExtension;
 import org.teavm.gradle.logging.LoggerWrapper;
+import org.teavm.model.ClassHolderTransformer;
+import org.teavm.tooling.ClassAlias;
+import org.teavm.tooling.MethodAlias;
 import org.teavm.tooling.TeaVMTool;
 import org.teavm.tooling.TeaVMToolException;
 import org.teavm.tooling.sources.DirectorySourceFileProvider;
 import org.teavm.tooling.sources.JarSourceFileProvider;
 
+/** Compiles TeaVM application. Expects the project to has the {@code mainClassName} property. Customizable with
+ * {@link TeaVMExtension}.
+ *
+ * @author MJ */
 public class CompileTask extends DefaultTask {
     @TaskAction
-    public void compile() throws TeaVMToolException {
+    public void compile() {
+        try {
+            compileTeaVM();
+        } catch (final Exception exception) {
+            getProject().getLogger().error("Unable to compile TeaVM application.", exception);
+            throw new GradleException("Unable to compile TeaVM application.", exception);
+        }
+    }
+
+    private void compileTeaVM() throws TeaVMToolException {
         final TeaVMTool tool = new TeaVMTool();
         tool.setLog(new LoggerWrapper(getProject().getLogger()));
         copyProperties(tool);
@@ -43,15 +80,20 @@ public class CompileTask extends DefaultTask {
     private void copyProperties(final TeaVMTool tool) throws TeaVMToolException {
         final TeaVMExtension extension = getProject().getExtensions().getByType(TeaVMExtension.class);
         tool.setTargetDirectory(toFile(extension.getTargetDirectory()));
+        tool.setTargetFileName(extension.getTargetFileName());
+        tool.setMinifying(extension.isMinifying());
+        tool.setDebugInformationGenerated(extension.isDebugInformationGenerated());
+        tool.setSourceMapsFileGenerated(extension.isSourceMapsGenerated());
+        tool.setSourceFilesCopied(extension.isSourceFilesCopied());
+        tool.setIncremental(extension.isIncremental());
         tool.setCacheDirectory(toFile(extension.getCacheDirectory()));
-        tool.setTargetFileName(extension.getTargetFile());
         tool.setRuntime(extension.getRuntime());
         tool.setMainPageIncluded(extension.isMainPageIncluded());
-        tool.setMinifying(extension.isMinify());
-        tool.setDebugInformationGenerated(extension.isDebugInfo());
-        tool.setSourceFilesCopied(extension.isSourceFilesCopied());
-        tool.setSourceMapsFileGenerated(extension.isSourceMaps());
         tool.setMainClass(findMainClass());
+        fillProperties(tool, extension);
+        fillClassAliases(tool, extension);
+        fillMethodAliases(tool, extension);
+        fillTransformers(tool, extension);
     }
 
     private File toFile(final String fileName) throws TeaVMToolException {
@@ -70,6 +112,52 @@ public class CompileTask extends DefaultTask {
             }
         }
         throw new TeaVMToolException("Unable to determine main class.");
+    }
+
+    private static void fillProperties(final TeaVMTool tool, final TeaVMExtension extension) {
+        if (extension.getProperties() != null) {
+            tool.getProperties().putAll(extension.getProperties());
+        }
+    }
+
+    private static void fillClassAliases(final TeaVMTool tool, final TeaVMExtension extension) {
+        if (extension.getClassAliases() != null) {
+            final List<ClassAlias> aliases = tool.getClassAliases();
+            for (final Entry<String, String> aliasData : extension.getClassAliases().entrySet()) {
+                final ClassAlias alias = new ClassAlias();
+                alias.setClassName(aliasData.getKey());
+                alias.setAlias(aliasData.getValue());
+                aliases.add(alias);
+            }
+        }
+    }
+
+    private static void fillMethodAliases(final TeaVMTool tool, final TeaVMExtension extension)
+            throws TeaVMToolException {
+        final List<MethodAlias> aliases = tool.getMethodAliases();
+        if (extension.getMethodAliases() != null) {
+            for (final Map<String, Object> aliasData : extension.getMethodAliases()) {
+                try {
+                    aliases.add(MethodAliasUtils.convert(aliasData));
+                } catch (final Exception exception) {
+                    throw new TeaVMToolException("Unable to construct method alias from: " + aliasData, exception);
+                }
+            }
+        }
+    }
+
+    private static void fillTransformers(final TeaVMTool tool, final TeaVMExtension extension)
+            throws TeaVMToolException {
+        if (extension.getTransformers() != null) {
+            final List<ClassHolderTransformer> transformers = tool.getTransformers();
+            for (final String transformerClass : extension.getTransformers()) {
+                try {
+                    transformers.add((ClassHolderTransformer) Class.forName(transformerClass).newInstance());
+                } catch (final Exception exception) {
+                    throw new TeaVMToolException("Unable to obtain transformer class: " + transformerClass, exception);
+                }
+            }
+        }
     }
 
     private void gatherSources(final TeaVMTool tool) {
