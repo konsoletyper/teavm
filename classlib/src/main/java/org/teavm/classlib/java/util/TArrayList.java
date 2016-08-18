b03ba67a884a0e14db7bb5dbf678cb9c8b2223a1
Sebastian Bauer <mail@sebastianbauer.info>
2016-08-18 20:09:48 +0200

    Use max() instead of min() when enlarging the array.
    Adding n elements subsequently has now an O(n) running time while it was
    O(n^2) before.

--- classlib/src/main/java/org/teavm/classlib/java/util/TArrayList.java
+++ classlib/src/main/java/org/teavm/classlib/java/util/TArrayList.java
@@ -53,7 +53,7 @@ public class TArrayList<E> extends TAbstractList<E> implements TCloneable, TSeri
 
     public void ensureCapacity(int minCapacity) {
         if (array.length < minCapacity) {
-            array = TArrays.copyOf(array, array.length + TMath.min(5, array.length / 2));
+            array = TArrays.copyOf(array, array.length + TMath.max(5, array.length / 2));
         }
     }
 
