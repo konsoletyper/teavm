de3727a4c0128e2ffd0a8502c05c5646043db35a
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2018-01-03 22:24:32 +0300

    Fix script that uploads CLI

--- update-cli.sh
+++ update-cli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl --ftp-create-dirs -T tools/cli/target/teavm-cli-dev-$NEW_VERSION.jar \
+curl --ftp-create-dirs -T tools/cli/target/teavm-cli-$NEW_VERSION.jar \
   -u $TEAVM_FTP_LOGIN:$TEAVM_FTP_PASSWORD \
-  ftp://$TEAVM_FTP_HOST/httpdocs/cli/dev/teavm-cli-dev-$NEW_VERSION.jar
+  ftp://$TEAVM_FTP_HOST/httpdocs/cli/dev/teavm-cli-$NEW_VERSION.jar
