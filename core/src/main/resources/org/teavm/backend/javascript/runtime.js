8f14d7d111c04564c2cff230e2a1221d593c0eb2
MaxGraey <maxgraey@gmail.com>
2019-09-30 20:09:02 +0300

    what if?

--- core/src/main/resources/org/teavm/backend/javascript/runtime.js
+++ core/src/main/resources/org/teavm/backend/javascript/runtime.js
@@ -24,7 +24,14 @@ function $rt_nextId() {
     return x;
 }
 function $rt_compare(a, b) {
-    return (a > b) - (a < b);
+    // return (a > b) - (a < b);
+    var nanA = isNaN(a);
+    var nanB = isNaN(b);
+    if (a === 0 && b === 0) {
+        a = 1 / a;
+        b = 1 / b;
+    }
+    return nanA && !nanB ? +1 : !nanA && nanB ? -1 : (a > b) - (a < b);
 }
 function $rt_isInstance(obj, cls) {
     return obj !== null && !!obj.constructor.$meta && $rt_isAssignable(obj.constructor, cls);
