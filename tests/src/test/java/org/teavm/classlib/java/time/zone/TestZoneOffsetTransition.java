07481653c57748d1bb48e39b196b6d5eec20c330
Jörg Hohwiller <hohwille@users.sourceforge.net>
2020-02-10 14:57:34 +0100

    fixed header for checkstyle

--- tests/src/test/java/org/teavm/classlib/java/time/zone/TestZoneOffsetTransition.java
+++ tests/src/test/java/org/teavm/classlib/java/time/zone/TestZoneOffsetTransition.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2020, adopted to TeaVM by Joerg Hohwiller
+ *  Copyright 2020 adopted to TeaVM by Joerg Hohwiller
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
