74b5518a8fff8ed32a66c37876d0baf8c61dbca9
Ihromant <vespertilion@gmail.com>
2020-06-13 17:59:21 +0300

    Added Collectors.toMap()

--- classlib/src/main/java/org/teavm/classlib/java/util/stream/TCollectors.java
+++ classlib/src/main/java/org/teavm/classlib/java/util/stream/TCollectors.java
@@ -17,11 +17,15 @@ package org.teavm.classlib.java.util.stream;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public final class TCollectors {
@@ -68,5 +72,71 @@ public final class TCollectors {
         };
         return TCollector.of(StringBuilder::new, accumulator, combiner,
                 sb -> sb.insert(0, prefix).append(suffix).toString());
+    }
+
+    public static <T, K, U> TCollector<T, ?, Map<K,U>> toMap(Function<? super T, ? extends K> keyMapper,
+            Function<? super T, ? extends U> valueMapper) {
+        return TCollector.of(HashMap::new,
+                uniqKeysMapAccumulator(keyMapper, valueMapper),
+                uniqKeysMapMerger(),
+                TCollector.Characteristics.IDENTITY_FINISH);
+    }
+
+    public static <T, K, U> TCollector<T, ?, Map<K,U>> toMap(Function<? super T, ? extends K> keyMapper,
+            Function<? super T, ? extends U> valueMapper,
+            BinaryOperator<U> mergeFunction) {
+        return toMap(keyMapper, valueMapper, mergeFunction, HashMap::new);
+    }
+
+    public static <T, K, U, M extends Map<K, U>> TCollector<T, ?, M> toMap(Function<? super T, ? extends K> keyMapper,
+            Function<? super T, ? extends U> valueMapper,
+            BinaryOperator<U> mergeFunction,
+            Supplier<M> mapFactory) {
+        BiConsumer<M, T> accumulator
+                = (map, element) -> map.merge(keyMapper.apply(element),
+                valueMapper.apply(element), mergeFunction);
+        return TCollector.of(mapFactory, accumulator, mapMerger(mergeFunction), TCollector.Characteristics.IDENTITY_FINISH);
+    }
+
+    private static <K, V, M extends Map<K,V>> BinaryOperator<M> uniqKeysMapMerger() {
+        return (m1, m2) -> {
+            for (Map.Entry<K,V> e : m2.entrySet()) {
+                K k = e.getKey();
+                V v = Objects.requireNonNull(e.getValue());
+                V u = m1.putIfAbsent(k, v);
+                if (u != null) {
+                    throw duplicateKeyException(k, u, v);
+                }
+            }
+            return m1;
+        };
+    }
+
+    private static <K, V, M extends Map<K,V>> BinaryOperator<M> mapMerger(BinaryOperator<V> mergeFunction) {
+        return (m1, m2) -> {
+            for (Map.Entry<K,V> e : m2.entrySet()) {
+                m1.merge(e.getKey(), e.getValue(), mergeFunction);
+            }
+            return m1;
+        };
+    }
+
+    private static <T, K, V> BiConsumer<Map<K, V>, T> uniqKeysMapAccumulator(Function<? super T, ? extends K> keyMapper,
+            Function<? super T, ? extends V> valueMapper) {
+        return (map, element) -> {
+            K k = keyMapper.apply(element);
+            V v = Objects.requireNonNull(valueMapper.apply(element));
+            V u = map.putIfAbsent(k, v);
+            if (u != null) {
+                throw duplicateKeyException(k, u, v);
+            }
+        };
+    }
+
+    private static IllegalStateException duplicateKeyException(
+            Object k, Object u, Object v) {
+        return new IllegalStateException(String.format(
+                "Duplicate key %s (attempted merging values %s and %s)",
+                k, u, v));
     }
 }
