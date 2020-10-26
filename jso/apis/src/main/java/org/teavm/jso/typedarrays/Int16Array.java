dfef5ffd24cc502b35730665fa10749a1c578852
Ivan Hetman <Ihromant@users.noreply.github.com>
2020-10-26 12:02:54 +0200

    JS: fix for creation of typed arrays from view method (#532)

--- jso/apis/src/main/java/org/teavm/jso/typedarrays/Int16Array.java
+++ jso/apis/src/main/java/org/teavm/jso/typedarrays/Int16Array.java
@@ -31,8 +31,8 @@ public abstract class Int16Array extends ArrayBufferView {
     @JSBody(params = "buffer", script = "return new Int16Array(buffer);")
     public static native Int16Array create(ArrayBuffer buffer);
 
-    @JSBody(params = "buffer", script = "return new Int16Array(buffer);")
-    public static native Int16Array create(ArrayBufferView buffer);
+    @JSBody(params = "bufferView", script = "return new Int16Array(bufferView.buffer);")
+    public static native Int16Array create(ArrayBufferView bufferView);
 
     @JSBody(params = { "buffer", "offset", "length" }, script = "return new Int16Array(buffer, offset, length);")
     public static native Int16Array create(ArrayBuffer buffer, int offset, int length);
