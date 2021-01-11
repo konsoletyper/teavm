8122095193a2c19a99a16d3d46b05ef9439c2168
Ivan Hetman <Ihromant@users.noreply.github.com>
2021-01-11 20:32:44 +0200

    Fix circular reference support in toString of collection classes (#552)

--- classlib/src/main/java/org/teavm/classlib/java/util/TAbstractCollection.java
+++ classlib/src/main/java/org/teavm/classlib/java/util/TAbstractCollection.java
@@ -142,15 +142,17 @@ public abstract class TAbstractCollection<E> extends TObject implements TCollect
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("[");
+        sb.append('[');
         TIterator<E> iter = iterator();
         if (iter.hasNext()) {
-            sb.append(String.valueOf(iter.next()));
+            E e = iter.next();
+            sb.append(e == this ? "(this Collection)" : iter.next());
         }
         while (iter.hasNext()) {
-            sb.append(", ").append(String.valueOf(iter.next()));
+            E e = iter.next();
+            sb.append(", ").append(e == this ? "(this Collection)" : iter.next());
         }
-        sb.append("]");
+        sb.append(']');
         return sb.toString();
     }
 }
