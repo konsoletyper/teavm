694a7e933558a39c01934bc522f5dcf7eb0734dc
LAX1DUDE <cgiacun@gmail.com>
2022-02-12 01:18:45 -0800

    Fixed backwards IndexedDB support check, it was throwing an exception only if IndexedDB IS SUPPORTED

--- jso/apis/src/main/java/org/teavm/jso/indexeddb/IDBFactory.java
+++ jso/apis/src/main/java/org/teavm/jso/indexeddb/IDBFactory.java
@@ -28,7 +28,7 @@ public abstract class IDBFactory implements JSObject {
 
     public static IDBFactory getInstance() {
         IDBFactory factory = getInstanceImpl();
-        if (!factory.isUndefined()) {
+        if (factory.isUndefined()) {
             throw new IllegalStateException("IndexedDB is not supported in this browser");
         }
         return factory;
