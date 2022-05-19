be259f1667e745bdfc822ed8fefb6016ee13e2ef
Alexey Andreev <konsoletyper@gmail.com>
2022-05-19 10:25:21 +0300

    Fix false negatives when checking missing methods

--- core/src/main/java/org/teavm/model/util/MissingItemsProcessor.java
+++ core/src/main/java/org/teavm/model/util/MissingItemsProcessor.java
@@ -56,7 +56,10 @@ public class MissingItemsProcessor {
     public void processClass(ClassHolder cls) {
         for (MethodHolder method : cls.getMethods()) {
             if (reachableMethods.contains(method.getReference()) && method.getProgram() != null) {
-                processMethod(method);
+                MethodDependencyInfo methodDep = dependencyInfo.getMethod(method.getReference());
+                if (methodDep != null && methodDep.isUsed()) {
+                    processMethod(method);
+                }
             }
         }
     }
