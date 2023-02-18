a4b2199142cfbe61219500282d6feca18cd938fb
Ivan Hetman <Ihromant@users.noreply.github.com>
2023-02-18 10:14:18 +0200

    jso apis: improve support of XMLHttpRequest

--- jso/apis/src/main/java/org/teavm/jso/websocket/WebSocket.java
+++ jso/apis/src/main/java/org/teavm/jso/websocket/WebSocket.java
@@ -35,7 +35,7 @@ public abstract class WebSocket implements JSObject {
   public abstract void onMessage(EventListener<MessageEvent> eventListener);
 
   @JSProperty("onopen")
-  public abstract void onOpen(EventListener<MessageEvent> eventListener);
+  public abstract void onOpen(EventListener<Event> eventListener);
 
   @JSBody(params = "url", script = "return new WebSocket(url);")
   public static native WebSocket create(String url);
