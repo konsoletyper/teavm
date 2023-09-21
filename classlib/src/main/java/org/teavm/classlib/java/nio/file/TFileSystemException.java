2ae9c593d2018ff2d1278fdb256a34d44485abb7
Alexey Andreev <konsoletyper@gmail.com>
2023-09-21 09:51:34 +0200

    classlib: satisfy checkstyle after applying latest PR

--- classlib/src/main/java/org/teavm/classlib/java/nio/file/TFileSystemException.java
+++ classlib/src/main/java/org/teavm/classlib/java/nio/file/TFileSystemException.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2023 Jonathan Coates.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.teavm.classlib.java.nio.file;
 
 import java.io.IOException;
@@ -36,12 +51,20 @@ public class TFileSystemException extends IOException {
 
     @Override
     public String getMessage() {
-        if (file == null && other == null) return reason;
+        if (file == null && other == null) {
+            return reason;
+        }
 
         StringBuilder out = new StringBuilder();
-        if (file != null) out.append(file);
-        if (other != null) out.append(" -> ").append(other);
-        if (reason != null) out.append(": ").append(reason);
+        if (file != null) {
+            out.append(file);
+        }
+        if (other != null) {
+            out.append(" -> ").append(other);
+        }
+        if (reason != null) {
+            out.append(": ").append(reason);
+        }
         return out.toString();
     }
 }
