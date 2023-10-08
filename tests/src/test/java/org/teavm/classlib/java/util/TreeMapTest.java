14ddeda8d61a31748f4fce3a86dd0b86d340e6bd
ihromant <vespertilion@gmail.com>
2023-10-09 01:02:23 +0300

    Minor change

--- tests/src/test/java/org/teavm/classlib/java/util/TreeMapTest.java
+++ tests/src/test/java/org/teavm/classlib/java/util/TreeMapTest.java
@@ -61,7 +61,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NavigableMap;
@@ -809,7 +808,7 @@ public class TreeMapTest {
 
     @Test
     public void testEmpty() {
-        var empty = new LinkedHashMap<>();
+        var empty = new TreeMap<>();
         assertNull(empty.pollFirstEntry());
         assertNull(empty.pollLastEntry());
         assertNull(empty.firstEntry());
