b36f38f48fa385e22a6fd861e87a395dec2ab502
Alexey Andreev <konsoletyper@gmail.com>
2024-09-05 09:08:53 +0200

    classlib: fix parsing floats and doubles with large exponents

--- tests/src/test/java/org/teavm/classlib/java/lang/DoubleTest.java
+++ tests/src/test/java/org/teavm/classlib/java/lang/DoubleTest.java
@@ -57,6 +57,7 @@ public class DoubleTest {
 
         assertEquals(4499999999999888888888888.0, Double.parseDouble("4499999999999888888888888"), 1E9);
         assertEquals(0.4499999999999888888888888, Double.parseDouble("0.4499999999999888888888888"), 1E-15);
+        assertEquals(Double.POSITIVE_INFINITY, Double.parseDouble("1e330"), 1E-15);
     }
 
     @Test
