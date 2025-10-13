2d11d120f2e5e1662d66568e89d9e3ffebc06e7b
Alexey Andreev <konsoletyper@gmail.com>
2025-10-13 17:47:34 +0200

    jso: implement mapping from Java fields to JS properties

--- jso/core/src/main/java/org/teavm/jso/JSTopLevel.java
+++ jso/core/src/main/java/org/teavm/jso/JSTopLevel.java
@@ -21,6 +21,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.METHOD, ElementType.TYPE })
+@Target({ ElementType.METHOD, ElementType.TYPE, ElementType.FIELD })
 public @interface JSTopLevel {
 }
