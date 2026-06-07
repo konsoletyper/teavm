e55d0a1bedbdf651ea02003822d7dc13fe77cbae
Alexey Andreev <konsoletyper@gmail.com>
2026-06-07 19:52:21 +0200

    c: temporarily unsuppress gdb output for debugging on CI

--- tests/run-process-unix-gdb.sh
+++ tests/run-process-unix-gdb.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 export LC_ALL=C.UTF-8
 SCRIPT_DIR=$(dirname "$0")
-gdb --batch-silent \
-  -ex 'set debuginfod enabled off' \
-  -ex 'set startup-with-shell off' \
-  -ex 'set verbose off' \
-  -ex 'set complaints 0' \
+echo About to run gdb
+gdb \
+  #--batch-silent \
+  #-ex 'set debuginfod enabled off' \
+  #-ex 'set startup-with-shell off' \
+  #-ex 'set verbose off' \
+  #-ex 'set complaints 0' \
   -ex run \
   -x "$SCRIPT_DIR/run-process-unix-gdb-commands.gdb" \
   --args "$@"
+echo gdb status: $GDB_STATUS
 GDB_STATUS=$?
 exit $GDB_STATUS
