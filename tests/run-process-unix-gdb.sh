9fff324983174be028c104c2925d385902d210c9
Alexey Andreev <konsoletyper@gmail.com>
2026-06-07 21:08:34 +0200

    c: trying to run gdb on CI

--- tests/run-process-unix-gdb.sh
+++ tests/run-process-unix-gdb.sh
@@ -3,11 +3,7 @@ export LC_ALL=C.UTF-8
 SCRIPT_DIR=$(dirname "$0")
 echo About to run gdb
 gdb \
-  #--batch-silent \
-  #-ex 'set debuginfod enabled off' \
-  #-ex 'set startup-with-shell off' \
-  #-ex 'set verbose off' \
-  #-ex 'set complaints 0' \
+  --batch \
   -ex run \
   -x "$SCRIPT_DIR/run-process-unix-gdb-commands.gdb" \
   --args "$@"
