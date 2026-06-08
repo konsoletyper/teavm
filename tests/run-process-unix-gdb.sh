707f59a735bd04376b47b45a8964d920f9721b85
Alexey Andreev <konsoletyper@gmail.com>
2026-06-08 18:46:55 +0200

    c: trying to fix gdb wrapper

--- tests/run-process-unix-gdb.sh
+++ tests/run-process-unix-gdb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export LC_ALL=C.UTF-8
 SCRIPT_DIR=$(dirname "$0")
-echo About to run gdb
 gdb \
-  --batch \
+  --batch-silent \
+  -ex 'set verbose off' \
+  -ex 'set complaints 0' \
   -ex run \
   -x "$SCRIPT_DIR/run-process-unix-gdb-commands.gdb" \
-  --args "$@"
-echo gdb status: $GDB_STATUS
+  --args env TEAVM_TEST_EXCEPTION_FILE="$TEAVM_TEST_EXCEPTION_FILE" "$@"
 GDB_STATUS=$?
 exit $GDB_STATUS
