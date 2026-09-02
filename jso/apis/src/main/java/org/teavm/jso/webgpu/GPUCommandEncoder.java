6b322ed432b482c994771b8cf97828fd3a4f11e7
Alexey Andreev <konsoletyper@gmail.com>
2026-09-02 18:51:06 +0200

    Fix checkstyle errors

--- jso/apis/src/main/java/org/teavm/jso/webgpu/GPUCommandEncoder.java
+++ jso/apis/src/main/java/org/teavm/jso/webgpu/GPUCommandEncoder.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2026 Alexey Andreev.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.teavm.jso.webgpu;
 
 public interface GPUCommandEncoder extends GPU.ObjectBase {
@@ -9,13 +24,17 @@ public interface GPUCommandEncoder extends GPU.ObjectBase {
 
     void copyBufferToBuffer(GPUBuffer source, GPUBuffer destination, double size);
 
-    void copyBufferToBuffer(GPUBuffer source, double sourceOffset, GPUBuffer destination, double destinationOffset, double size);
+    void copyBufferToBuffer(GPUBuffer source, double sourceOffset, GPUBuffer destination, double destinationOffset,
+            double size);
 
-    void copyBufferToTexture(GPUDescriptor.TexelCopyBufferInfo source, GPUDescriptor.TexelCopyTextureInfo destination, GPUDescriptor.Extent3D copySize);
+    void copyBufferToTexture(GPUDescriptor.TexelCopyBufferInfo source, GPUDescriptor.TexelCopyTextureInfo destination,
+            GPUDescriptor.Extent3D copySize);
 
-    void copyTextureToBuffer(GPUDescriptor.TexelCopyTextureInfo source, GPUDescriptor.TexelCopyBufferInfo destination, GPUDescriptor.Extent3D copySize);
+    void copyTextureToBuffer(GPUDescriptor.TexelCopyTextureInfo source, GPUDescriptor.TexelCopyBufferInfo destination,
+            GPUDescriptor.Extent3D copySize);
 
-    void copyTextureToTexture(GPUDescriptor.TexelCopyTextureInfo source, GPUDescriptor.TexelCopyTextureInfo destination, GPUDescriptor.Extent3D copySize);
+    void copyTextureToTexture(GPUDescriptor.TexelCopyTextureInfo source, GPUDescriptor.TexelCopyTextureInfo destination,
+            GPUDescriptor.Extent3D copySize);
 
     void clearBuffer(GPUBuffer buffer);
 
@@ -23,7 +42,8 @@ public interface GPUCommandEncoder extends GPU.ObjectBase {
 
     void clearBuffer(GPUBuffer buffer, double offset, double size);
 
-    void resolveQuerySet(GPUDevice.QuerySet querySet, int firstQuery, int queryCount, GPUBuffer destination, double destinationOffset);
+    void resolveQuerySet(GPUDevice.QuerySet querySet, int firstQuery, int queryCount, GPUBuffer destination,
+            double destinationOffset);
 
     CommandBuffer finish();
 
--- jso/apis/src/main/java/org/teavm/jso/webgpu/GPUQueue.java
+++ jso/apis/src/main/java/org/teavm/jso/webgpu/GPUQueue.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2026 Alexey Andreev.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.teavm.jso.webgpu;
 
 import org.teavm.jso.core.JSArrayReader;
@@ -23,9 +38,12 @@ public interface GPUQueue extends GPU.ObjectBase {
 
     void writeBuffer(GPUBuffer buffer, double bufferOffset, ArrayBufferView data, double dataOffset, double size);
 
-    void writeTexture(GPUDescriptor.TexelCopyTextureInfo destination, ArrayBuffer data, GPUDescriptor.TexelCopyBufferLayout dataLayout, GPUDescriptor.Extent3D size);
+    void writeTexture(GPUDescriptor.TexelCopyTextureInfo destination, ArrayBuffer data,
+            GPUDescriptor.TexelCopyBufferLayout dataLayout, GPUDescriptor.Extent3D size);
 
-    void writeTexture(GPUDescriptor.TexelCopyTextureInfo destination, ArrayBufferView data, GPUDescriptor.TexelCopyBufferLayout dataLayout, GPUDescriptor.Extent3D size);
+    void writeTexture(GPUDescriptor.TexelCopyTextureInfo destination, ArrayBufferView data,
+            GPUDescriptor.TexelCopyBufferLayout dataLayout, GPUDescriptor.Extent3D size);
 
-    void copyExternalImageToTexture(GPUDescriptor.CopyExternalImageSourceInfo source, GPUDescriptor.CopyExternalImageDestInfo destination, GPUDescriptor.Extent3D copySize);
+    void copyExternalImageToTexture(GPUDescriptor.CopyExternalImageSourceInfo source,
+            GPUDescriptor.CopyExternalImageDestInfo destination, GPUDescriptor.Extent3D copySize);
 }
