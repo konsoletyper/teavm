559ec1a4ee387179d9536d333cad01ddbe1db9e0
Carl Stainton <carls@Mini2025.local>
2026-09-08 21:14:37 +0100

    Initialise suppressed exceptions in Throwable replacement constructors

--- tests/src/test/java/org/teavm/classlib/java/lang/ThrowableTest.java
+++ tests/src/test/java/org/teavm/classlib/java/lang/ThrowableTest.java
@@ -16,6 +16,7 @@
 package org.teavm.classlib.java.lang;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -36,5 +37,74 @@ public class ThrowableTest {
     public void toStringWorks() {
         assertEquals("java.lang.RuntimeException: fail", new RuntimeException("fail").toString());
         assertEquals("java.lang.RuntimeException", new RuntimeException().toString());
+    }
+
+    @Test
+    public void suppressedExceptionsInitiallyEmpty() {
+        for (Throwable exception : createThrowables()) {
+            assertEquals(0, exception.getSuppressed().length);
+        }
+    }
+
+    @Test
+    public void suppressedExceptionsRetained() {
+        Throwable first = new Throwable("first");
+        Throwable second = new Throwable("second");
+        for (Throwable exception : createThrowables()) {
+            exception.addSuppressed(first);
+            exception.addSuppressed(second);
+            assertEquals(2, exception.getSuppressed().length);
+            assertSame(first, exception.getSuppressed()[0]);
+            assertSame(second, exception.getSuppressed()[1]);
+        }
+    }
+
+    @Test
+    public void suppressedExceptionsReturnedAsCopy() {
+        Throwable exception = new Throwable();
+        Throwable suppressed = new Throwable("suppressed");
+        exception.addSuppressed(suppressed);
+        exception.getSuppressed()[0] = null;
+        assertSame(suppressed, exception.getSuppressed()[0]);
+    }
+
+    @Test
+    public void suppressionCanBeDisabled() {
+        for (boolean writableStackTrace : new boolean[] {false, true}) {
+            Throwable exception = new ConfigurableThrowable(false, writableStackTrace);
+            assertEquals(0, exception.getSuppressed().length);
+            exception.addSuppressed(new Throwable("ignored"));
+            assertEquals(0, exception.getSuppressed().length);
+        }
+    }
+
+    @Test
+    public void tryWithResourcesRetainsCloseFailure() {
+        RuntimeException primary = new RuntimeException("primary");
+        RuntimeException secondary = new RuntimeException("close");
+        try (AutoCloseable resource = () -> {
+            throw secondary;
+        }) {
+            throw primary;
+        } catch (Exception exception) {
+            assertSame(primary, exception);
+            assertEquals(1, exception.getSuppressed().length);
+            assertSame(secondary, exception.getSuppressed()[0]);
+        }
+    }
+
+    private Throwable[] createThrowables() {
+        Throwable cause = new Throwable("cause");
+        return new Throwable[] {
+                new Throwable(), new Throwable("message"), new Throwable(cause),
+                new Throwable("message", cause), new ConfigurableThrowable(true, true),
+                new ConfigurableThrowable(true, false)
+        };
+    }
+
+    private static class ConfigurableThrowable extends Throwable {
+        ConfigurableThrowable(boolean enableSuppression, boolean writableStackTrace) {
+            super("configured", null, enableSuppression, writableStackTrace);
+        }
     }
 }
